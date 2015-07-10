f6aa46cce1060780752266aab811869c616ac87b
mariano ferle <marianoferle@gmail.com>
2015-07-10 19:46:32 -0300

    Despliegue en Heroku

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
+
 
 var routes = require('./routes/index');
 
@@ -14,6 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -23,7 +27,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ controllers/quiz_controller.js
@@ -0,0 +1,17 @@
+
+//GET /quizer/question
+
+exports.question=function(req, res){
+    res.render('quizes/question',{pregunta: 'Capital de Italia'});
+};
+
+
+//GET /quizer/answer
+
+exports.answer=function(req, res){
+  if(req.query.respuesta === 'Roma'){
+      res.render('quizes/answer',{respuesta: 'Correcto'});
+  }else{
+      res.render('quizes/answer',{respuesta: 'Incorrecto'});
+  }
+};
