1d9c72dd6327ab51866c18faed7c920230342d68
mariano ferle <marianoferle@gmail.com>
2015-07-11 19:13:33 -0300

    Despliegue DB en Heroku

---
+++ models/quiz.js
@@ -0,0 +1,9 @@
+// Definicion del modelo de Quiz con validación
+
+module.exports = function(sequelize, DataTypes){
+  return sequelize.define('Quiz',{ 
+     pregunta: DataTypes.STRING,
+     respuesta: DataTypes.STRING,     
+   });
+
+}
