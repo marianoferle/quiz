6a0c13c5036507d8ef11382dbda659ea2e069ebe
mariano ferle <marianoferle@gmail.com>
2015-07-13 22:39:11 -0300

    credit author

--- models/models.js
+++ models/models.js
@@ -36,16 +36,15 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; 
 
 // sync crea automaticamente la BD inicializa tabla y base de datos
-sequelize.sync().success(function() {
-    Quiz.count().success(function (count){
-		if(count===0){
-			   Quiz.create({
-			           pregunta: 'Capital de Italia',
-			           respuesta: 'Roma'
-			   }).success(function(){			           
-			           console.log('Base de datos inicializada')
-			   });
+
+//inicializa las tablas
+sequelize.sync().then(function() {
+	//then() ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function (count){
+		if(count===0){  // la tabla se inicializa solo si esta vacia.
+			  Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'});
+    		  Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa'})
+    		  .then(function(){ console.log('Base de datos inicializada'); });
 		};
     });
 });
-
